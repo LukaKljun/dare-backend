8170abd8fb9a843584dcffa26732f19f60d25b00
luka kljun <luka.kljun@students.vegova.si>
2024-09-27 17:55:55 +0200

    dee

--- api/index.js
+++ api/index.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
+app.use(cors()); // Enable CORS to allow requests from your app
 
-// Enable CORS
-app.use(cors());
-app.use(express.json());
-
+// Sample list of dares
 const dares = [
-    "Take a picture with your favorite book!",
-    "Show off your best dance move!",
-    "Take a picture while doing a handstand (or trying to)!",
+  "Take a picture with your favorite book!",
+  "Show off your best dance move!",
+  "Take a photo with something that makes you laugh!",
+  "Pretend you're a famous athlete and snap a picture while 'winning'!",
+  // Add more dares as needed
 ];
-  
 
-// Route to get the daily dare
+// Endpoint to get a random dare
 app.get('/daily-dare', (req, res) => {
-    const startOfYear = new Date(new Date().getFullYear(), 0, 1);
-    const dayOfYear = Math.floor((new Date() - startOfYear) / (1000 * 60 * 60 * 24));
-    const dare = dares[dayOfYear % dares.length]; // Rotate dares
-    res.json({ dare });
+  const randomDare = dares[Math.floor(Math.random() * dares.length)];
+  res.json({ dare: randomDare });
 });
 
-// Start the server
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// Export the app as a serverless function
+module.exports = app;
